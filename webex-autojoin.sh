88e0766c98845a79dc4896157c794c01e242e573
Athul P <athul929@gmail.com>
2021-01-21 02:33:46 +0530

    Update webex-autojoin.sh

--- webex-autojoin.sh
+++ webex-autojoin.sh
@@ -4,6 +4,4 @@ URL=$1
 PROFILE="headless"
 
 echo "Opening Firefox"
-/usr/bin/firefox -P $PROFILE -headless $TOKEN $URL &
-pid=$!
-echo "Waiting for join"
+/usr/bin/firefox -P $PROFILE -headless $URL &
